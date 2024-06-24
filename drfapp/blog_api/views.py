1d311e1bf210a4dc9568989b03d637bae8d46a18
Aj connect <ajiriiviero2021@gmail.com>
2024-06-24 13:20:37 +0200

    kamruj auth

---
+++ drfapp/blog_api/views.py
@@ -0,0 +1,50 @@
+from rest_framework import generics
+from blog.models import Post
+from .serializers import PostSerializer
+from rest_framework.permissions import IsAdminUser,  BasePermission, SAFE_METHODS, DjangoModelPermissions
+
+
+#permissions for user to able to edit/custom permissions
+class PostUserWritePermission(BasePermission):
+    message = 'Editing post is restricted to the author only.'
+
+    def has_object_permission(self, request, view, obj):
+
+        if request.method in SAFE_METHODS:
+            return True
+
+        return obj.author == request.user
+
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [DjangoModelPermissions]
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView, PostUserWritePermission):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [PostUserWritePermission]
+
+
+""" Concrete View Classes
+#CreateAPIView
+Used for create-only endpoints.
+#ListAPIView
+Used for read-only endpoints to represent a collection of model instances.
+#RetrieveAPIView
+Used for read-only endpoints to represent a single model instance.
+#DestroyAPIView
+Used for delete-only endpoints for a single model instance.
+#UpdateAPIView
+Used for update-only endpoints for a single model instance.
+##ListCreateAPIView
+Used for read-write endpoints to represent a collection of model instances.
+RetrieveUpdateAPIView
+Used for read or update endpoints to represent a single model instance.
+#RetrieveDestroyAPIView
+Used for read or delete endpoints to represent a single model instance.
+#RetrieveUpdateDestroyAPIView
+Used for read-write-delete endpoints to represent a single model instance.
+"""
